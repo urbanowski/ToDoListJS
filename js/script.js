79db7f83b6368a11edf19d432f2790014c4e3777
Marek Urbanowski <marek.urbanowski@hotmail.com>
2018-07-26 23:43:07 +0200

    Added delete button to item

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 $("document").ready(function(){
+    
+    //Add Task Event
     $("#add").click(function(){
         var taskValue = $("#taskValue").val();
         if (taskValue !== ""){
-            AddTask(taskValue);
+            AddTask(taskValue);            
+            
+            //Clear input field
             ClearInputField();
+
+            //Delete Task Event
+            $(".deleteBtn").click(function(){
+                $(this).parent().remove();
+                }
+            );
         }
     });
-})
+});
 
 function AddTask(task){
     var item = $("<li></li>").text(task);
+    
+    //Add "Delete" Button to item
+    var delBtn = $("<button class='deleteBtn'></button>").text("X");
+    item.append(delBtn);
+
     $("#taskList").append(item);
 }
 
 function ClearInputField(){
-    $("#taskValue").val() = "";
+    $("#taskValue").val("");
 }
